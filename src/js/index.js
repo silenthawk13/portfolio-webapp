a9c5606e43493fb07de6c8d3462cc57f1aaff24f
Waithaka <jwaithaka746@gmail.com>
2022-07-10 13:35:56 +0300

    updated my resume

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import initTilt from "./tilt";
 import initSr from "./sr";
 import { addResume } from "./utils";
-import resume from "../assets/resume.pdf";
+import resume from "../assets/John Waithaka.pdf";
 
 export default function initApp() {
   initSr();
